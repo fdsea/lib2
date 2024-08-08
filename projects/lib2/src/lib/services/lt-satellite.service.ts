1985f1c4fbda78c00f1a350d665cc5b8626af2cd
Vladimir Kukov <vladimir.kukov@intistele.com>
2024-08-08 16:44:18 +0300

    wip

--- projects/lib2/src/lib/services/lt-satellite.service.ts
+++ projects/lib2/src/lib/services/lt-satellite.service.ts
@@ -3,7 +3,8 @@ import {SatelliteDroparea} from 'satellite-droparea';
 
 @Injectable({providedIn: 'root'})
 export class LtSatelliteService {
-  instance: SatelliteDroparea | null = null;;
+  instance: SatelliteDroparea | null = null;
+  
   init() {
     this.instance = new SatelliteDroparea({} as any)
   }
